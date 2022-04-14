3a88f8cb9f370e317d30491dd700dedd4c1d45c5
Enrique Viña Alonso <alu0101337760@ull.edu.es>
2022-04-14 14:21:29 +0100

    VRP class with buildFromFile aparently working

--- DAA_VRP/DAA_VRP/Program.cs
+++ DAA_VRP/DAA_VRP/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace DAA_VRP
+{
+    class Program
+    {
+        public static void Main()
+        {
+            // Create a new instance of the problem.
+            VRP vrp = new VRP();
+
+            string testFile = "C:\\Users\\enriq\\source\\repos\\P07_DAA_VRP\\DAA_VRP\\DAA_VRP\\I40j_2m_S1_1.txt";
+
+            vrp.BuildFromFile(testFile);
+
+        }
+    }
+}
+
---
+++ DAA_VRP/DAA_VRP/VRP.cs
@@ -0,0 +1,45 @@
+﻿namespace DAA_VRP
+{
+    public class VRP
+    {
+        int numberOfClients = -1;
+        int numberOfVehicles = -1;
+        List<List<int>> distanceMatrix = new List<List<int>>();
+
+        private int RetrieveNumberOfClients(string line)
+        {
+            string[] split = line.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+            return int.Parse(split[1]);
+
+        }
+
+        private int RetrieveNumberOfVehicles(string line)
+        {
+            string[] split = line.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+            return int.Parse(split[1]);
+        }
+
+        private void buildDistanceMatrix(List<string> lines)
+        {
+            for (int i = 0; i < numberOfClients; i++)
+            {
+                List<int> row = new List<int>();
+                for (int j = 0; j < numberOfClients; j++)
+                {
+                    row.Add(int.Parse(lines[i].Split(new char[0], StringSplitOptions.RemoveEmptyEntries)[j]));
+                }
+                distanceMatrix.Add(row);
+            }
+        }
+
+        public void BuildFromFile(string filename)
+        {
+            List<string> lines = new List<string>(File.ReadAllLines(filename));
+            this.numberOfClients = RetrieveNumberOfClients(lines[0]);
+            this.numberOfVehicles = RetrieveNumberOfVehicles(lines[1]);
+            buildDistanceMatrix(lines.GetRange(3, lines.Count - 4));
+
+        }
+
+    }
+}
